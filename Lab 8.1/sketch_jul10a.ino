06d8b02f7363612252c8d1b6e925ddde92d15365
Jerrod Bolton <85716225+JerrodBolton@users.noreply.github.com>
2025-07-10 16:05:34 -0700

    Added 8.1

---
+++ Lab 8.1/sketch_jul10a.ino
@@ -0,0 +1,44 @@
+#include <ArduinoJson.h>
+#include <IOXhop_FirebaseESP32.h>
+#include <IOXhop_FirebaseStream.h>
+#include <WiFi.h>
+#include <M5Core2.h>
+#include "secret.h"
+
+String fireStatus = "";
+
+void setup() {
+  Serial.begin(9600);
+  M5.begin();
+  // connect to wifi.
+  WiFi.begin(SECRET_SSID, SECRET_PASS);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+    Serial.println();
+    Serial.print("connected: ");
+    Serial.println(WiFi.localIP());
+  }
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.setString("LED_STATUS", "OFF");
+}
+void loop() {
+  // 1. Read the database
+  String fireStatus = Firebase.getString("LED_STATUS");
+  fireStatus.trim();
+  fireStatus.toUpperCase();
+
+  // 2. Act on it
+  if (fireStatus == "OFF") {
+    M5.Lcd.fillScreen(WHITE);
+  } else if (fireStatus == "ON") {
+    M5.Lcd.fillScreen(YELLOW);
+  } else {
+    Serial.print("Unexpected value: ");
+    Serial.println(fireStatus);
+  }
+
+  // 3. Don’t hammer Firebase
+  delay(300);   // 0.3 s pause
+}
